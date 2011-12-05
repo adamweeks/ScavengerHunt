ce1f13cb307b022be9b764eb21f86fe2131eec6d
Adam Weeks <adam.weeks@gmail.com>
2011-12-05 16:21:47 -0500

    Initial Commit

---
+++ ScavengerHunt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScavengerHunt
+//
+//  Created by Adam Weeks on 12/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "aws_AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([aws_AppDelegate class]));
+    }
+}
